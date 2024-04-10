dba66fe838f7b84b6f0b776175ef5376b809b100
Whythem <mathiasmilhec@gmail.com>
2024-04-10 16:06:26 +0200

    AppelApi

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner/spinner"
 
 export default function Home() {
+  const [manhwa, setManhwa] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const url = 'https://webtoon.p.rapidapi.com/canvas/home?language=en';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '8be1034410msh86769c4d94a197ap19fbd1jsnd0c1de49152a',
+        'X-RapidAPI-Host': 'webtoon.p.rapidapi.com'
+      }
+    };
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        setManhwa(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
+  }, [])
   return(
     <>
-    
+      {
+        loading ?
+          <Spinner/>
+        :
+        <>
+          {console.log(manhwa)}
+        </>
+      }
     </>
   )
 }
